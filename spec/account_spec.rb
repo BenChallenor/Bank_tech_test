03a01c5fa6f1934bc529b39d3ac697c8860732be
Ben <ben.challenor@btopenworld.com>
2018-03-26 13:17:55 +0100

    Added gemfile and test for initail balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,1 +1,11 @@
-require "account"
+require 'account'
+
+describe 'Account' do
+  account = Account.new
+
+  describe 'Initialize' do
+    it 'Should create an initial balance of 0' do
+      expect(account.balance).to eq 0
+    end
+  end
+end
